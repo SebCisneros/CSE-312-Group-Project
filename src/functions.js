6374da0a977ea976805a71fd07045c2e62c98360
ztlaistn <54722655+ztlaistn@users.noreply.github.com>
2022-04-08 12:57:28 -0400

    Added registration page and basic confirmation functionality

--- src/functions.js
+++ src/functions.js
@@ -1,1 +1,16 @@
-//write js functions here
+//Registration functions
+function confirm_registration(){
+    let email = document.getElementById("email_input");
+    if(email.value === ""){
+        window.alert("Please enter your email.");
+    }
+    let username = document.getElementById("username_input");
+    let password = document.getElementById("password_input");
+    let password_confirmation = document.getElementById("password_confirmation");
+    if(password.value === password_confirmation.value){
+        window.location.href = "/login";
+    } else {
+        document.getElementById("registration-form").event.preventDefault();
+        window.alert("The passwords do not match. Please re-enter your password.");
+    }
+}
